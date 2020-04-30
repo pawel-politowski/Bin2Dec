da4280ff7b12212c0aed5e91057c9d52ba097d05
Paweł Politowski <matelly@poczta.onet.pl>
2020-04-30 12:45:48 +0200

    Bin2Dec and Dec2Bin done

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function convertBinToDec() {
     });
     const decimal = parseInt(binary, 2);
     document.querySelector('#decimalOutput').value = decimal;
-    return true;
+    return true
+}
+
+function convertDecToBin() {
+    const decimal2 = document.querySelector('#decimalInput').value;
+    if (decimal2 === '' || typeof Number(decimal2) !== "number") {return alert('Please, enter a decimal number')};
+    const binary2 = (decimal2 >>> 0).toString(2)
+    document.querySelector('#binaryOutput').value = binary2;
+    return true
 }
 
